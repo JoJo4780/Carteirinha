d0fd8fd5369c82cdbee5aa0bf2a0f541ddf87f88
JoJo4780 <zarakik11111@gmail.com>
2023-11-08 20:55:20 -0300

    Prototipo

--- Carteirinha/wwwroot/js/site.js
+++ Carteirinha/wwwroot/js/site.js
@@ -1,35 +1,26 @@
 ﻿document.getElementById('User_image').addEventListener('change', function (event) {
     const imagePreview = document.getElementById('preview_Image');
     var input_image = document.getElementById('Url_image');
+    var preview = document.getElementById('preview');
     const file = event.target.files[0];
-    var image_hide = document.querySelectorAll(".image")
-    console.log(image_hide)
-    image_hide.forEach(function (e) {
-        e.style.display = "none"
-    })
+    var image_hide = document.getElementById("mensagem_image")
+    var icone = document.getElementById("Icone")
+    image_hide.style.display = "none"
+    icone.style.display = "none"
     if (file) {
         // Leitura do arquivo e exibição da imagem
         const reader = new FileReader();
         reader.onload = function (e) {
             imagePreview.src = e.target.result;
             input_image.value = e.target.result;
+            console.log(e.target.result)
             imagePreview.style.display = 'block';
+            preview.style.display = 'block';
+            
         };
         reader.readAsDataURL(file);
     } else {
         imagePreview.style.display = 'none';
         imagePreview.src = '#';
     }
-});
-import { Tabulator } from 'https://unpkg.com/tabulator-tables@5.5.2/dist/js/tabulator_esm.min.js';
-var table = new Tabulator("#example-table", {
-    height: "311px",
-    columns: [
-        { title: "Name", field: "name" },
-        { title: "Progress", field: "progress", sorter: "number" },
-        { title: "Gender", field: "gender" },
-        { title: "Rating", field: "rating" },
-        { title: "Favourite Color", field: "col" },
-        { title: "Date Of Birth", field: "dob", hozAlign: "center" },
-    ],
 });
